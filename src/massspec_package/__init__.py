2a0c27b9023b6cb827cf2e8519b28b75180dadd8
Mercury2211 <iseli.alex.5@gmail.com>
2025-02-17 15:09:33 +0100

    Delete src/massspec_package directory

--- src/massspec_package/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-Mass Spectrometry Data Processing Package
-"""
-
-from .data_processor import DataProcessor
-from .voltage_plotter import VoltagePlotter
-from .gui import App
-
-__version__ = "0.1.0"
